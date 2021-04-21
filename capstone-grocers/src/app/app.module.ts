17dd8d26447d9ae3f5fce7b3507d7bf9c11543c9
nicolas <nicolas.tamez@icloud.com>
2021-04-21 15:34:41 -0500

    added components for user panel

--- capstone-grocers/src/app/app.module.ts
+++ capstone-grocers/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserPanelComponent } from './Components/user/user-panel/user-panel.component';
+import { SignInComponent } from './Components/user/sign-in/sign-in.component';
+import { SignUpComponent } from './Components/user/sign-up/sign-up.component';
+import { SelectItemComponent } from './Components/user/select-item/select-item.component';
+import { ViewCartComponent } from './Components/user/view-cart/view-cart.component';
+import { ViewOrderStatusComponent } from './Components/user/view-order-status/view-order-status.component';
+import { ProfileComponent } from './Components/user/profile/profile.component';
+import { CheckoutComponent } from './Components/user/checkout/checkout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserPanelComponent,
+    SignInComponent,
+    SignUpComponent,
+    SelectItemComponent,
+    ViewCartComponent,
+    ViewOrderStatusComponent,
+    ProfileComponent,
+    CheckoutComponent
   ],
   imports: [
     BrowserModule,
