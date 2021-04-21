0fcd2f4b5ccee4d1bc292e0e1300dc22404a309a
darrenadixonpi <darrenadixonpi@gmail.com>
2021-04-21 16:37:52 -0400

    Populating models on the backend -Darren

--- backEnd/models/orderModel.js
+++ backEnd/models/orderModel.js
@@ -3,8 +3,14 @@ const mongoose = require('mongoose');
 
 let schema = mongoose.Schema;
 
+
 let order = new schema({
-    
+    _id:String,
+    u_username:String,
+    cart:String,                //stringified JSON of user's cart (Product[])
+    datetime_requested:String,
+    datetime_fulfilled:String,
+    status:String               //can be either "in-progress" or "fulfilled"
 });
 
 module.exports = mongoose.model('Order',order);
--- backEnd/models/ticketModel.js
+++ backEnd/models/ticketModel.js
@@ -4,7 +4,11 @@ const mongoose = require('mongoose');
 let schema = mongoose.Schema;
 
 let ticket = new schema({
-    
+    _id:String,
+    u_username:String,              //user who raised the ticket
+    datetime_raised:String,
+    datetime_resolved:String,
+    status:String                   //can be either "in-progress" or "resolved"
 });
 
 module.exports = mongoose.model('Ticket',ticket);
