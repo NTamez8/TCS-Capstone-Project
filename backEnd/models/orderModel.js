7d196dc2217c801aabb64c09668060e5649c10c5
srushtipatel <sap353@scarletmail.rutgers.edu>
2021-04-21 15:43:28 -0500

    order part upadted

--- backEnd/models/orderModel.js
+++ backEnd/models/orderModel.js
@@ -4,7 +4,9 @@ const mongoose = require('mongoose');
 let schema = mongoose.Schema;
 
 let order = new schema({
-    
+    _id:Number,
+    status:String
+
 });
 
 module.exports = mongoose.model('Order',order);
--- backEnd/routes/orderRoute.js
+++ backEnd/routes/orderRoute.js
@@ -2,6 +2,6 @@ const express = require('express');
 const orderController = require('../controllers/orderController');
 const routes = express.Router();
 
-
-
+router.get("/retriveOrderById/:oid",orderController.getOrderById)
+router.get("/updateProductStatus",orderController.updateOrderByStatus)
 module.exports = routes;
