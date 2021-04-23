ba79806033443963fa9835f1609c0dfca23a6367
nicolas <nicolas.tamez@icloud.com>
2021-04-23 15:14:27 -0500

    order by prod only returns products now

--- backEnd/controllers/orderController.js
+++ backEnd/controllers/orderController.js
@@ -1,38 +1,44 @@
 const order = require('../models/orderModel');
 
-let getOrderById=(req,res)=>{
-    let oid=req.params.oid;     //passing through path param
-    order.find({_id:oid},(err,data)=>{
-        if(!err){
-            res.json(data);    //return array
+let getOrderById = (req, res) => {
+    let oid = req.params.oid; //passing through path param
+    order.find({
+        _id: oid
+    }, (err, data) => {
+        if (!err) {
+            res.json(data); //return array
         }
     })
 }
 
 
-let updateOrderByStatus=(req,res)=>{
-    let pid=req.body.pid;
-    let updatedstatus=req.body.status
-    order.updateMany({_id:pid},{$set:{status:updatedstatus}},(err,result)=>{
-        if(!err){
-            if(result.nModified>0){
-            res.send("Order status updated succesfully"+result)
+let updateOrderByStatus = (req, res) => {
+    let pid = req.body.pid;
+    let updatedstatus = req.body.status
+    order.updateMany({
+        _id: pid
+    }, {
+        $set: {
+            status: updatedstatus
+        }
+    }, (err, result) => {
+        if (!err) {
+            if (result.nModified > 0) {
+                res.send("Order status updated succesfully" + result)
 
-            if(status=="cancelled"){
-               // res.send(user.funds);
-               var a = 1000;
-               var b = 50;
-               var sum = a+b;
-               console.log(sum)
-            }
+                if (status == "cancelled") {
+                    // res.send(user.funds);
+                    var a = 1000;
+                    var b = 50;
+                    var sum = a + b;
+                    console.log(sum)
+                }
 
-            }
-            else{
+            } else {
                 res.send("Order is not Upadated")
             }
-        }
-        else{
-            res.send("Error generated "+err);
+        } else {
+            res.send("Error generated " + err);
         }
     })
 }
@@ -41,7 +47,7 @@ let updateOrderByStatus=(req,res)=>{
 //     let orderdetails = new order({
 //         _id:req.body.oid,
 //         status:req.body.status,
-        
+
 //     });
 //     orderdetails.save((err,result)=>{
 //         if(!err){
@@ -51,111 +57,113 @@ let updateOrderByStatus=(req,res)=>{
 //             res.send("Order Didn't upadte"+err)
 //         }
 
-    
+
 //     })
 
 // }
 
 
-let getOrdersByDay = async (req,res,next) =>
-{
-    try
-    {
+let getOrdersByDay = async (req, res, next) => {
+    try {
         let day = req.body.date;
-        let foundOrders = await order.find({datetime_requested:day});
+        let foundOrders = await order.find({
+            datetime_requested: day
+        });
         res.send(foundOrders);
-    }
-    catch(err)
-    {
+    } catch (err) {
         next(err)
     }
 }
 
-function addDay(date,day)
-{
+function addDay(date, day) {
     var result = new Date(date);
     result.setDate(result.getDate() + days);
     return result;
 }
 
-let getOrdersByWeek = async (req,res,next) =>
-{
-    try
-    {
-        let weekStart = req.body.dateStart;      
-        
-        let weekEnd = addDay(weekStart,7).toLocaleDateString();
-        let foundOrders = await order.find({datetime_requested:{$gte:weekStart, $lte:weekEnd}});
+let getOrdersByWeek = async (req, res, next) => {
+    try {
+        let weekStart = req.body.dateStart;
+
+        let weekEnd = addDay(weekStart, 7).toLocaleDateString();
+        let foundOrders = await order.find({
+            datetime_requested: {
+                $gte: weekStart,
+                $lte: weekEnd
+            }
+        });
         res.send(foundOrders);
-    }
-    catch(err)
-    {
+    } catch (err) {
         next(err)
     }
 }
 
-let getOrdersByMonth = async (req,res,next) =>
-{
-    try
-    {
-        let monthStart = req.body.monthStart;      
+let getOrdersByMonth = async (req, res, next) => {
+    try {
+        let monthStart = req.body.monthStart;
         let date = new Date(monthStart);
         let month = date.getMonth();
         let year = '';
-        if(month == 12)
-        {
+        if (month == 12) {
             month = 1;
-            year = date.getFullYear()+1;
+            year = date.getFullYear() + 1;
+        } else {
+            month++;
+            year = date.getFullYear();
         }
-        else{
-            month ++;
-            year = date.getFullYear();}
-        let nextMonth = new Date(year,month,day);
-        let foundOrders = await order.find({datetime_requested:{$gte:monthStart, $lt:nextMonth.toLocaleDateString()}});
+        let nextMonth = new Date(year, month, day);
+        let foundOrders = await order.find({
+            datetime_requested: {
+                $gte: monthStart,
+                $lt: nextMonth.toLocaleDateString()
+            }
+        });
         res.send(foundOrders);
-    }
-    catch(err)
-    {
+    } catch (err) {
         next(err)
     }
 }
 
-let getOrdersByProduct = async (req,res,next) =>
-{
-    try
-    {
+let getOrdersByProduct = async (req, res, next) => {
+    try {
         let productId = req.params.id;
-        let ordersByProd = await order.find({"cart.product":productId}).populate('cart.product');
-       
-       
+        let ordersByProd = await order.find({},{
+           // "cart.product": productId
+           cart:{$elemMatch:{product:productId}}
+        }).populate('cart.product');
+
+
         res.send(ordersByProd);
 
-    }
-    catch(err)
-    {
+    } catch (err) {
         next(err)
     }
 }
 
-let getOrdersByCust = async (req,res,next) =>
-{
-    try
-    {
+let getOrdersByCust = async (req, res, next) => {
+    try {
         let custId = req.params.id;
-        let ordersByProd = await order.find({user_ID:custId}).populate('cart.product');
-        if(!ordersByProd)
-        {
+        let ordersByProd = await order.find({
+            user_ID: custId
+        }).populate('cart.product');
+        if (!ordersByProd) {
             const error = new Error("Wrong credentials");
             error.statusCode = 401;
             throw error;
         }
         res.send(ordersByProd);
-    }
-    catch(err)
-    {
+    } catch (err) {
         next(err)
     }
 }
 
 
-module.exports = {getOrderById,updateOrderByStatus,getOrdersByDay,getOrdersByWeek,getOrdersByMonth,getOrdersByCust,getOrdersByProduct}
+module.exports = {
+    getOrderById,
+    updateOrderByStatus,
+    getOrdersByDay,
+    getOrdersByWeek,
+    getOrdersByMonth,
+    getOrdersByCust,
+    getOrdersByProduct
+}
