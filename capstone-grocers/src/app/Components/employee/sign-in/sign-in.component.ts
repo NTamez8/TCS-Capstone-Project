b15ec54d2638aebfc4fd6eacaf36be7440f8a237
pruthviraj <pruthvirajmandala@gmail.com>
2021-04-24 19:09:10 -0500

    logout component added

--- capstone-grocers/src/app/Components/employee/sign-in/sign-in.component.ts
+++ capstone-grocers/src/app/Components/employee/sign-in/sign-in.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { EmployeeService } from 'src/app/Services/employee.service';
 
 @Component({
   selector: 'app-sign-in',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignInComponent implements OnInit {
 
-  constructor() { }
+  constructor(private empServ:EmployeeService) { }
 
   ngOnInit(): void {
+  }
+
+  login(empRef:any)
+  {
+    
+    this.empServ.signIn(empRef.userName,empRef.password).subscribe(data=>{
+      console.log(data.token);
+      sessionStorage.setItem('token',data.token);
+    })
   }
 
 }
