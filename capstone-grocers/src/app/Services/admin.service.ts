9fe306d24f39129d24e4ff2e6c926b3c8b93bd77
darrenadixonpi <darrenadixonpi@gmail.com>
2021-04-26 19:07:10 -0400

    Fixing routing error from adminSignInComponent

--- capstone-grocers/src/app/Services/admin.service.ts
+++ capstone-grocers/src/app/Services/admin.service.ts
@@ -12,22 +12,22 @@ export class AdminService {
     return this.http.post<{token:string}>('http://localhost:8080/admin/signIn',{a_username,a_password});
   };
 
-  // validate(token:string):Promise<boolean>{
-  //   return new Promise<boolean>((resolve, reject) => {
+  validate(token:string):Promise<boolean>{
+    return new Promise<boolean>((resolve, reject) => {
 
-  //     // try changing the response type to text and seeing how that effects the response
-  //     this.http.get('http://localhost:8080/admin/isValid', {
-  //       headers: {
-  //         "Authorization": token
-  //       },
-  //       responseType:'text'
-  //     }).subscribe((data)=>{
-  //       resolve(true);
-  //     },error=>{
-  //       reject(error);
-  //     });
+      // try changing the response type to text and seeing how that effects the response
+      this.http.get('http://localhost:8080/admin/isValid', {
+        headers: {
+          "Authorization": token
+        },
+        responseType:'text'
+      }).subscribe((data)=>{
+        resolve(true);
+      },error=>{
+        reject(error);
+      });
 
-  //   });
-  // }
+    });
+  };
 
 }
