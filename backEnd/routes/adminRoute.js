ac44dec419fc6ded11005e7f06851c0983433fa1
darrenadixonpi <darrenadixonpi@gmail.com>
2021-04-26 18:18:17 -0400

    Modifying adminRoute.js

--- backEnd/routes/adminRoute.js
+++ backEnd/routes/adminRoute.js
@@ -3,7 +3,11 @@ const adminController = require('../controllers/adminController');
 const adminAuth = require('../middleware/adminPassport')();
 const routes = express.Router();
 
-routes.post("/signIn",adminController.signIn);
+//GET
 routes.get("/isValid",adminAuth.authenticate(),adminController.isValid);
+routes.get("/getMe",adminAuth.authenticate(),adminController.getMe);
+
+//POST
+routes.post("/signIn",adminController.signIn);
 
 module.exports = routes;
