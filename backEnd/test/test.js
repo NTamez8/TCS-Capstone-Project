6c6f40e51a20443660fdd351d20634f9e59172c7
nicolas <nicolas.tamez@icloud.com>
2021-04-27 13:56:51 -0500

    starting on test

--- backEnd/test/test.js
+++ backEnd/test/test.js
@@ -1,7 +1,28 @@
 var assert = require('assert');
-
+const adminController = require('../controllers/adminController');
+const employeeController = require('../controllers/employeeController');
+const orderController = require('../controllers/orderController');
+const productController = require('../controllers/productController');
+const requestController = require('../controllers/requestController');
+const userController = require('../controllers/userController');
 let chai = require('chai');
 let http = require('chai-http');
 let server = require('../index');
 let should = chai.should();
 chai.use(http);
+
+describe('Admin',()=>{
+
+    it('Should sign in',(done)=>{
+        let adminCredentials = {
+            "a_username":"admin",
+            "a_password":"AdminCapstone"
+        }
+        chai.request(server).post('/admin/signIn',adminCredentials).end((err,result)=>{
+            result.should.have.status(200);
+            console.log(result);
+        })
+    })
+
+
+})
