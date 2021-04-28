7b60b30bf48ef8eb6d884f0d7074cae6affa7136
darrenadixonpi <darrenadixonpi@gmail.com>
2021-04-28 19:17:37 -0400

    Now Force-removing all requests with invalid product ID's

--- capstone-grocers/src/app/Components/admin/view-requests/view-requests.component.ts
+++ capstone-grocers/src/app/Components/admin/view-requests/view-requests.component.ts
@@ -4,6 +4,7 @@ import { Router } from '@angular/router';
 import { productRequest } from 'src/app/Classes/request';
 import { ProductService } from 'src/app/Services/product.service';
 import { RequestService } from 'src/app/Services/request.service';
+import { ViewProductsComponent } from 'src/app/Components/admin/view-products/view-products.component';
 
 
 @Component({
@@ -26,7 +27,7 @@ export class ViewRequestsComponent implements OnInit {
 
   async getRequestById(requestRef:NgForm){
     //update Requests before checking for valid ID
-    this.getAllRequests();
+    await this.getAllRequests();
     let request_order = requestRef.value.request_order;
     let request_id = this.requestService.currentRequests[request_order-1]?._id;
     //check for valid Request ID
@@ -46,50 +47,61 @@ export class ViewRequestsComponent implements OnInit {
   };
 
   async resolveRequest(request:productRequest){
-    if(this.productService.productExists(request.product_id)){
-      //console.log("Resolving request!");
-      let productService = this.productService;
-      let requestService = this.requestService;
-      let curComponent = this;
-      await productService.getProductById(request.product_id as string).subscribe(
-        function (result){
-          productService.currentProducts=result;
-          if(productService.currentProducts[0].quantity != request.new_quantity){
-            alert(`Please change product quantity! Current quantity: ${productService.currentProducts[0].quantity}`);
-            //console.log(productService.currentProducts);
-          //otherwise we can automatically resolve the request
-          }else{
-            requestService.resolveRequest(request._id as string).subscribe(result=>console.log(result.token),error=>console.log(error));
-            curComponent.getAllRequests();
-          }
-        },error=>console.log(error));
-    }else{
-      alert("The request must be removed because the product ID no longer exists!\nNow will delete all requests with invalid product ID's.");
-      let thisReference = this;
-      this.requestService.currentRequests.forEach(
-        function(request){
-          thisReference.deleteRequestForce(request);
-        });
-    }
+    let productService = this.productService;
+    let requestService = this.requestService;
+    let curComponent = this;
+    await productService.getAllProducts().subscribe(function(result){
+      productService.currentProducts=result;
+      if(productService.productExists(request.product_id)){
+        //console.log("Resolving request!");
+        productService.getProductById(request.product_id as string).subscribe(
+          function (result){
+            productService.currentProducts=result;
+            if(productService.currentProducts[0].quantity != request.new_quantity){
+              alert(`Please change product quantity! Current quantity: ${productService.currentProducts[0].quantity}`);
+              //console.log(productService.currentProducts);
+            //otherwise we can automatically resolve the request
+            }else{
+              requestService.resolveRequest(request._id as string).subscribe(result=>console.log(result.token),error=>console.log(error));
+              curComponent.getAllRequests();
+            }
+          },error=>console.log(error));
+      }else{
+        alert("The request must be removed because the product ID no longer exists!\nNow will delete all requests with invalid product ID's.");
+        requestService.currentRequests.forEach(
+          function(request){
+            console.log(!curComponent.productService.productExists(request.product_id));
+            if(!productService.productExists(request.product_id)){
+              curComponent.deleteRequestForce(request);
+          }});
+      }
+    });
   };
 
   //Remove only resolved requests
   async deleteRequest(request:productRequest){
-    if(this.productService.productExists(request.product_id)){
-      if(request.status=="resolved"){
-        await this.requestService.deleteRequestById(request._id as string).subscribe(data=>console.log(data.token));
-        this.getAllRequests();
+    let curComponent = this;
+    let productService = this.productService;
+    let requestService = this.requestService;
+    await productService.getAllProducts().subscribe(function(result){
+      productService.currentProducts=result;
+      if(productService.productExists(request.product_id)){
+        if(request.status=="resolved"){
+          requestService.deleteRequestById(request._id as string).subscribe(data=>console.log(data.token));
+          curComponent.getAllRequests();
+        }else{
+          alert("You cannot remove a request until it is resolved!");
+        }
       }else{
-        alert("You cannot remove a request until it is resolved!");
-      }
-    }else{
-      alert("The request must be removed because the product ID no longer exists!\nNow will delete all requests with invalid product ID's.");
-      let thisReference = this;
-      this.requestService.currentRequests.forEach(
-        function(request){
-          thisReference.deleteRequestForce(request);
-        });
-    }
+        alert("The request must be removed because the product ID no longer exists!\nNow will delete all requests with invalid product ID's.");
+        requestService.currentRequests.forEach(
+          function(request){
+            console.log(!curComponent.productService.productExists(request.product_id));
+            if(!curComponent.productService.productExists(request.product_id)){
+              curComponent.deleteRequestForce(request);
+          }});
+      };
+    });
   };
 
   //Force-delte requests (ones that have an invalid product_id)
