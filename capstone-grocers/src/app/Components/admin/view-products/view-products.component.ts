bb22c27c283cfe83db8d62c3028dc7b814c4f7ca
darrenadixonpi <darrenadixonpi@gmail.com>
2021-04-28 21:59:03 -0400

    Adding product viewing comments

--- capstone-grocers/src/app/Components/admin/view-products/view-products.component.ts
+++ capstone-grocers/src/app/Components/admin/view-products/view-products.component.ts
@@ -18,32 +18,38 @@ export class ViewProductsComponent implements OnInit {
     this.getAllProducts();
   }
 
+  // get product index
   getProductIndex(product:Product){
+    // get index from product service
     return this.productService.currentProducts.findIndex((product)=>this.productService.currentProduct[0]._id == product._id);
   }
 
+  // get all products
   async getAllProducts(){
-    //console.log("Getting products");
+    // viewing all products
     this.single = false;
     await this.productService.getAllProducts().subscribe(result=>this.productService.currentProducts=result,error=>console.log(error));
   }
 
+  // get product by id
   async getProductById(productRef:NgForm){
-    //update Products before checking for valid ID
+    // update Products before checking for valid ID
     let product_order = productRef.value.product_order;
     this.getAllProducts();
     let product_id = this.productService.currentProducts[product_order-1]?._id;
-    //check for valid Product ID
+    // check for valid Product ID
     if(this.productService.productExists(product_id as String)){
       await this.productService.getProductById(product_id as String).subscribe(result=>this.productService.currentProduct=result,error=>console.log(error));
+      // now viewing a single product
       this.single = true;
-      //console.log(this.productService)
     }else{
       alert("Product does not exist!");
     }
   };
 
+  // delete product
   async deleteProduct(product:Product){
+    // delete the product from the product service then refresh products
     await this.productService.deleteProductById(product._id as string).subscribe(data=>{
       alert(data.message);
       this.getAllProducts();
