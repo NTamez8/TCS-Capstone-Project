7aea2405b710a1cc25dce2c45d4c873dca2ce5ed
nicolas <nicolas.tamez@icloud.com>
2021-04-29 11:19:09 -0500

    fixed config to use testDB

--- backEnd/config/dbConfig.js
+++ backEnd/config/dbConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-    url:'mongodb://localhost:27017/capstone',
+    url:'mongodb://localhost:27017/capstoneTest',
     options:{ useNewUrlParser: true,
         useUnifiedTopology: true}
 }
