c7bde605f9cee19d6af5d764b66f002057fc4f42
darrenadixonpi <darrenadixonpi@gmail.com>
2021-04-28 22:05:07 -0400

    Adding admin signin comments

--- capstone-grocers/src/app/Components/admin/sign-in/sign-in.component.ts
+++ capstone-grocers/src/app/Components/admin/sign-in/sign-in.component.ts
@@ -16,11 +16,13 @@ export class SignInComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  // try to sign-in
   a_signIn(a_loginRef:NgForm){
     const a_credentials = a_loginRef.value;
     this.adminService.signIn(a_credentials.a_username,a_credentials.a_password).subscribe(data=>{
+      // if there is a message, then sign-in was successful
       if(data.message){
-        //alert(data.message);
+        // alert(data.message);
       }else{
         alert("Invalid credentials");
       }
