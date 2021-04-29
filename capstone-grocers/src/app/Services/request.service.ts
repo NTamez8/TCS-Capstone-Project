bc9a6a788bdfbf02487a113afabf9b2f0807af6c
darrenadixonpi <darrenadixonpi@gmail.com>
2021-04-29 16:54:49 -0400

    Cleaning request service code and comments

--- capstone-grocers/src/app/Services/request.service.ts
+++ capstone-grocers/src/app/Services/request.service.ts
@@ -8,12 +8,15 @@ import { productRequest } from '../Classes/request';
 })
 export class RequestService {
 
-  public currentRequest:Array<productRequest> = [];
+  // Request URL
   public viewRequestURL:any;
+  // both need to be Arrays because of how Observables are returned from the HttpClient
+  public currentRequest:Array<productRequest> = [];
   public currentRequests:Array<productRequest> = [];
 
   constructor(private http:HttpClient) { }
 
+  // test if a Request exists based off of its ID
   requestExists(request_id:String):Boolean{
     if(request_id){
       let requestExists = false;
@@ -28,32 +31,38 @@ export class RequestService {
     }
   };
 
+  // get all Requests
   getAllRequests():Observable<productRequest[]>{
+    // sending Admin validation to backend
     let token = 'bearer ' + sessionStorage.getItem('adminToken');
     return this.http.get<productRequest[]>("http://localhost:8080/request/getAllRequests",{headers:{'Authorization':token}});
   }
 
+  // get Request based on ID
   getRequestById(request_id:any):Observable<productRequest[]>{
+    // sending Admin validation to backend
     let token = 'bearer ' + sessionStorage.getItem('adminToken');
     return this.http.get<productRequest[]>("http://localhost:8080/request/getRequestById/"+request_id,{headers:{'Authorization':token}});
   }
 
   addRequest(){}
 
- 
-
-
+  // send a Request
   sendRequest(productRef:any){
     // changed response type
     return this.http.post("http://localhost:8080/request/sendRequest",productRef,{responseType:"text"});
   }
 
+  // resolve a Request
   resolveRequest(request_id:string){
+    // sending Admin validation to backend
     let token = 'bearer ' + sessionStorage.getItem('adminToken');
     return this.http.post<{token:string}>("http://localhost:8080/request/resolveRequest",{request_id},{headers:{'Authorization':token}});
   }
 
+  // delete a Request
   deleteRequestById(request_id:String){
+    // sending Admin validation to backend
     let token = 'bearer ' + sessionStorage.getItem('adminToken');
     return this.http.post("http://localhost:8080/request/deleteRequestById",{request_id},{headers:{"Authorization":token} ,responseType:'text'});
   }
