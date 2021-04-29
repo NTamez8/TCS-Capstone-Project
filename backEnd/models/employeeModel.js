3bc85252abadb621a2d23b0a270cbfad76b94e31
srushtipatel <sap353@scarletmail.rutgers.edu>
2021-04-29 12:02:18 -0400

    adding presentation

--- backEnd/models/employeeModel.js
+++ backEnd/models/employeeModel.js
@@ -1,3 +1,34 @@
+// const mongoose = require('mongoose');
+// const bcrypt = require('bcryptjs');
+
+// let schema = mongoose.Schema;
+
+// let employee = new schema({
+//     firstName:String,
+//     lastName:String,
+//     email_address:String,
+//     e_password:String,          //must be auto-generated when employee is first added by the admin
+//     first_login:Boolean
+// });
+// employee.methods.encryptPassword = async password =>{
+  
+//     const salt = await bcrypt.genSalt(5);
+  
+//     const hash = await bcrypt.hash(password,salt);
+   
+//     return hash;
+// };
+
+// //BORROWED FROM "userModel.js", thanks :) - Darren
+// employee.methods.validPassword = async function(candidatePassword){
+//     console.log(candidatePassword);
+// employee.methods.validPassword = async function(candidatePassword){
+//     const result = await bcrypt.compare(candidatePassword,this.e_password);
+//     return result;
+// };
+
+// module.exports = mongoose.model('Employee',employee);
+
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
@@ -10,10 +41,7 @@ let employee = new schema({
     e_password:String,          //must be auto-generated when employee is first added by the admin
     first_login:Boolean
 });
-<<<<<<< HEAD
-//BORROWED FROM "userModel.js", thanks :) - Darren
-=======
->>>>>>> 60e8452d30f25002896cb6ae5ece7c46b16c71f7
+
 employee.methods.encryptPassword = async password =>{
   
     const salt = await bcrypt.genSalt(5);
@@ -23,13 +51,9 @@ employee.methods.encryptPassword = async password =>{
     return hash;
 };
 
-<<<<<<< HEAD
-//BORROWED FROM "userModel.js", thanks :) - Darren
+
 employee.methods.validPassword = async function(candidatePassword){
-    console.log(candidatePassword);
-=======
-employee.methods.validPassword = async function(candidatePassword){
->>>>>>> 60e8452d30f25002896cb6ae5ece7c46b16c71f7
+
     const result = await bcrypt.compare(candidatePassword,this.e_password);
     return result;
 };
